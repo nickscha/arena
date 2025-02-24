bc35eb45f00b61d512349c203b16c54c4e5ff995
nickscha <nick@scharnbeck.de>
2025-02-24 16:40:42 +0100

    [FIX] Fallback to stdlib for macos

--- arena.h
+++ arena.h
@@ -108,7 +108,7 @@ static ARENA_INLINE void arena_free_win32(void *ptr)
 static void *(*allocator_default)(unsigned long) = arena_malloc_win32;
 static void (*deallocator_default)(void *) = arena_free_win32;
 
-#elif defined(__linux__) || defined(__APPLE__)
+#elif defined(__linux__)
 
 #include <sys/mman.h>
 #include <unistd.h>
